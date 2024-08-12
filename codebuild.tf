54769980cf144662c3c49078e1a70efbdd483ca3
Shahid Mahmood <shahidmahmood.yar@gmail.com>
2024-08-12 15:57:29 +0500

    updated profile to prod

--- codebuild.tf
+++ codebuild.tf
@@ -1,7 +1,7 @@
 resource "aws_codebuild_project" "build_project" {
   name          = "codebuild-project"
   description   = "CodeBuild project for CodePipeline"
-  build_timeout = "5"
+  # build_timeout = "5"
 
   service_role = aws_iam_role.codebuild_role.arn
 
